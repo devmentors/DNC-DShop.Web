eaecf13e75c4275ed9fa69a55239f959100626d1
Dariusz Pawlukiewicz <foreverframenet@gmail.com>
2018-05-16 13:31:29 +0200

    Added base api service and auth service

---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,23 @@
+const accessTokenKey = 'access_token';
+
+export class AuthService {
+
+  private isSessionStored: boolean;
+
+  get storage() {
+    return this.isSessionStored ? window.sessionStorage : window.localStorage;
+  }
+
+  setAccessToken(accessToken: string, isSessionStored: boolean): void {
+    this.isSessionStored = isSessionStored;
+    this.storage.setItem(accessTokenKey, accessToken);
+  }
+
+  getAccessToken(): string {
+    return this.storage.getItem(accessTokenKey);
+  }
+
+  removeAccessToken(): void {
+    this.storage.removeItem(accessTokenKey);
+  }
+}
--- src/app/shared/base-api.service.ts
+++ src/app/shared/base-api.service.ts
@@ -1,13 +1,45 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpRequest } from 'selenium-webdriver/http';
+import { AuthService } from './auth.service';
 
-export class BaseApiService {
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  }),
+  body: {}
+};
 
-  private readonly hostUrl = 'http://localhost:5000';
+const hostUrl = 'http://localhost:5000';
 
-  constructor(private http: HttpClient) {}
+export abstract class BaseApiService {
 
-  protected get<TData>(url: string, isProtected: boolean = false) {
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
+  protected get<TData>(url: string, isProtected: boolean = false): Observable<TData> {
+    return this.request('GET', url, isProtected);
+  }
+
+  protected post<TData>(url: string, data: TData, isProtected: boolean = false): Observable<TData> {
+    return this.request('POST', url, isProtected, data);
+  }
+
+  protected put<TData>(url: string, data: TData, isProtected: boolean = false): Observable<TData> {
+    return this.request('GET', url, isProtected, data);
+  }
+
+  protected delete<TData>(url: string, isProtected: boolean = false): Observable<TData> {
+    return this.request('GET', url, isProtected);
+  }
+
+  private request<TData>(method: string, url: string, isProtected: boolean, data?: TData): Observable<TData> {
+
+    if (isProtected) {
+      const token = this.authService.getAccessToken();
+      httpOptions.headers.append('Authorization', token);
+    }
+
+    httpOptions.body = data;
+    return this.http.request<TData>(method, `${hostUrl}/${url}`, httpOptions);
   }
 }
